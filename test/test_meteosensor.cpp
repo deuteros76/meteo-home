c511402feac72bc2dad708c66e3c2fcf29d55650
Guillermo Amat <deuteros76@gmail.com>
2023-09-30 17:55:10 +0200

    refactor: test changed to use std::vector for storing object pointers (as in the project source code)

--- test/test_meteosensor.cpp
+++ test/test_meteosensor.cpp
@@ -5,6 +5,7 @@
 #include <mhdht.hpp>
 #include <mhsgp30.hpp>
 #include <mhbmp.hpp>
+#include <vector>
 
 //Temperature sensor settings
 #define DHTPIN 12 // what digital pin we're connected to
@@ -14,6 +15,8 @@ Manager manager;  //! Portal and wific connection manager
 //MQTT client
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+std::vector<std::unique_ptr<MeteoSensor>> sensors; // To store sensors addresses
 
 void setup(void) {
     // set stuff up here
@@ -42,41 +45,29 @@ void test_createSGPDiscoveryMsg() {
     TEST_ASSERT_TRUE(test_message.equals(sensor.getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::co2_sensor)));
 }
 
-void test_objectIteration() {
-    uintptr_t sensors[]={};
-    int sensorIndex=0;
-
+void test_objectIteration() {    
     MHDHT dht(DHTPIN, DHTTYPE);
     MHBMP bmp;
     MHSGP30 sgp30;
+    
+    sensors.emplace_back(&dht);
+    sensors.emplace_back(&bmp);
+    sensors.emplace_back(&sgp30);
 
-    sensors[sensorIndex]=reinterpret_cast<uintptr_t>(&dht);
-    sensorIndex++;
-    sensors[sensorIndex]=reinterpret_cast<uintptr_t>(&bmp);
-    sensorIndex++;
-    sensors[sensorIndex]=reinterpret_cast<uintptr_t>(&sgp30);
-
-
-    MeteoSensor* sensor = reinterpret_cast <MeteoSensor*> (sensors[0]);
     String test_message("{\"name\":\"sensor.meteohome-test_topic-DHT22-temperature\",\"stat_cla\":\"measurement\",\"dev_cla\":\"temperature\",\"stat_t\":\"test_topic/DHT22/temperature\",\"unit_of_meas\":\"ºC\",\"frc_upd\":true,\"uniq_id\":\"test_topic/DHT22/temperature\"}");
-    TEST_ASSERT_TRUE(test_message.equals(sensor->getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::temperature_sensor)));
+    TEST_ASSERT_TRUE(test_message.equals(sensors.at(0)->getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::temperature_sensor)));
  
-    sensor = reinterpret_cast <MeteoSensor*> (sensors[1]);
     test_message="{\"name\":\"sensor.meteohome-test_topic-BMP180-pressure\",\"stat_cla\":\"measurement\",\"dev_cla\":\"atmospheric_pressure\",\"stat_t\":\"test_topic/BMP180/pressure\",\"unit_of_meas\":\"Pa\",\"frc_upd\":true,\"uniq_id\":\"test_topic/BMP180/pressure\"}";
-    TEST_ASSERT_TRUE(test_message.equals(sensor->getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::pressure_sensor)));
+    TEST_ASSERT_TRUE(test_message.equals(sensors.at(1)->getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::pressure_sensor)));
 
-    sensor = reinterpret_cast <MeteoSensor*> (sensors[2]);
     test_message="{\"name\":\"sensor.meteohome-test_topic-SGP30-co2\",\"stat_cla\":\"measurement\",\"dev_cla\":\"carbon_dioxide\",\"stat_t\":\"test_topic/SGP30/co2\",\"unit_of_meas\":\"ppm\",\"frc_upd\":true,\"uniq_id\":\"test_topic/SGP30/co2\"}";
-    TEST_ASSERT_TRUE(test_message.equals(sensor->getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::co2_sensor)));
+    TEST_ASSERT_TRUE(test_message.equals(sensors.at(2)->getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::co2_sensor)));
 
-    sensor = reinterpret_cast <MeteoSensor*> (sensors[0]);
-    TEST_ASSERT_FALSE(test_message.equals(sensor->getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::co2_sensor)));
+    test_message="{\"name\":\"sensor.meteohome-test_topic\",\"stat_cla\":\"measurement\",\"dev_cla\":\"test\",\"stat_t\":\"test_topic\",\"unit_of_meas\":\"%\",\"frc_upd\":true,\"uniq_id\":\"test_topic\"}";
 
-    /*
-    for (int i =0; i<sensorIndex; i++){
-        MeteoSensor* sensor = reinterpret_cast <MeteoSensor*> (sensors[i]);
+    for (auto &sensor : sensors) {
         TEST_ASSERT_TRUE(test_message.equals(sensor->createDiscoveryMsg("test_topic","test","%")));
-    }*/
+    }
 }
 
 //int main( int argc, char **argv) {
