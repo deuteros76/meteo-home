5b27d45b6f4b870f88625cb18b114d04ccc89389
Guillermo Amat <deuteros76@gmail.com>
2023-11-10 19:50:40 +0100

    fix: tests adapted to the refactored classes

--- test/test_meteosensor.cpp
+++ test/test_meteosensor.cpp
@@ -28,27 +28,34 @@ void tearDown(void) {
 
 void test_createDHTDiscoveryMsg() {
 
-    MHDHT sensor(DHTPIN, DHTTYPE);
+    MeteoBoard board(&manager, &client);
+    MHDHT sensor(&board, &manager,DHTPIN, DHTTYPE); 
     String test_message("{\"name\":\"sensor.meteohome-test_topic-DHT22-temperature\",\"stat_cla\":\"measurement\",\"dev_cla\":\"temperature\",\"stat_t\":\"test_topic/DHT22/temperature\",\"unit_of_meas\":\"ºC\",\"frc_upd\":true,\"uniq_id\":\"test_topic/DHT22/temperature\"}");
     TEST_ASSERT_TRUE(test_message.equals(sensor.getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::temperature_sensor)));
 }
 
 void test_createBMPDiscoveryMsg() {
-    MHBMP sensor;
+    MeteoBoard board(&manager, &client);
+    MHBMP sensor(&board, &manager);
     String test_message("{\"name\":\"sensor.meteohome-test_topic-BMP180-pressure\",\"stat_cla\":\"measurement\",\"dev_cla\":\"atmospheric_pressure\",\"stat_t\":\"test_topic/BMP180/pressure\",\"unit_of_meas\":\"Pa\",\"frc_upd\":true,\"uniq_id\":\"test_topic/BMP180/pressure\"}");
     TEST_ASSERT_TRUE(test_message.equals(sensor.getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::pressure_sensor)));
 }
 
 void test_createSGPDiscoveryMsg() {
-    MHSGP30 sensor;
+    MeteoBoard board(&manager, &client);
+    Leds leds; 
+    MHSGP30 sensor(&board, &manager, &leds);
     String test_message("{\"name\":\"sensor.meteohome-test_topic-SGP30-co2\",\"stat_cla\":\"measurement\",\"dev_cla\":\"carbon_dioxide\",\"stat_t\":\"test_topic/SGP30/co2\",\"unit_of_meas\":\"ppm\",\"frc_upd\":true,\"uniq_id\":\"test_topic/SGP30/co2\"}");
     TEST_ASSERT_TRUE(test_message.equals(sensor.getDiscoveryMsg("test_topic",MeteoSensor::deviceClass::co2_sensor)));
 }
 
 void test_objectIteration() {    
-    MHDHT dht(DHTPIN, DHTTYPE);
-    MHBMP bmp;
-    MHSGP30 sgp30;
+    MeteoBoard board(&manager, &client);
+    Leds leds; 
+    
+    MHDHT dht(&board, &manager,DHTPIN, DHTTYPE);
+    MHBMP bmp(&board, &manager);
+    MHSGP30 sgp30(&board, &manager, &leds);
     
     sensors.emplace_back(&dht);
     sensors.emplace_back(&bmp);
